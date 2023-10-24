fc8df94197579d8f5a38116a4ba3e9a3db01ff1b
Fabricio Cavalcanti <fabriciocavalcanti97@gmail.com>
2023-10-24 11:01:19 -0300

    Initial commit

---
+++ src/Banco.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Banco {
+    private String nome;
+    private List<Conta> contas;
+}
+
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente fabricio = new Cliente();
+		fabricio.setNome("Fabricio");
+		
+		Conta cc = new ContaCorrente(fabricio);
+		Conta poupanca = new ContaPoupanca(fabricio);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
