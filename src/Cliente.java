f954855beb4f3221e22eb16bfa706af306f9f5e1
Fabricio Cavalcanti <fabriciocavalcanti97@gmail.com>
2023-10-24 15:45:09 -0300

    New features implemented

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,14 +1,62 @@
-
 public class Cliente {
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String email;
 
-	private String nome;
+    public Cliente(String nome, String endereco, String telefone, String email) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    // Construtor vazio
+    public Cliente() {
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Método para atualizar informações de contato
+    public void atualizarInformacoesDeContato(String novoEndereco, String novoTelefone, String novoEmail) {
+        if (novoEndereco != null) {
+            this.endereco = novoEndereco;
+        }
+        if (novoTelefone != null) {
+            this.telefone = novoTelefone;
+        }
+        if (novoEmail != null) {
+            this.email = novoEmail;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,52 @@
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		Cliente fabricio = new Cliente();
-		fabricio.setNome("Fabricio");
-		
-		Conta cc = new ContaCorrente(fabricio);
-		Conta poupanca = new ContaPoupanca(fabricio);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
-	}
+        Cliente fabricio = new Cliente();
+        fabricio.setNome("Fabricio");
 
+        Conta cc = new ContaCorrente(fabricio);
+        Conta poupanca = new ContaPoupanca(fabricio);
+
+        cc.depositar(1000);
+        cc.imprimirExtrato();
+
+        poupanca.depositar(100);
+        poupanca.imprimirExtrato();
+
+        // Menu de operações
+        while (true) {
+            System.out.println("\nEscolha uma operação:");
+            System.out.println("1. Sacar");
+            System.out.println("2. Transferir");
+            System.out.println("3. Sair");
+
+            int escolha = Integer.parseInt(scanner.nextLine());
+
+            switch (escolha) {
+                case 1:
+                    System.out.println("Digite o valor para sacar da Conta Corrente:");
+                    double valorSaque = Double.parseDouble(scanner.nextLine());
+                    cc.sacar(valorSaque);
+                    cc.imprimirExtrato();
+                    break;
+                case 2:
+                    System.out.println("Digite o valor para transferir da Conta Corrente para a Poupança:");
+                    double valorTransferencia = Double.parseDouble(scanner.nextLine());
+                    cc.transferir(valorTransferencia, poupanca);
+                    cc.imprimirExtrato();
+                    poupanca.imprimirExtrato();
+                    break;
+                case 3:
+                    System.out.println("Saindo do programa.");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        }
+    }
 }
